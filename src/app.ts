e5feecf5fecd1d0445f80ffdd44c4a1f57699576
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-29 13:37:03 -0400

    ➕ Add and setup npm packages in app

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,34 @@
-import express, { Express } from "express"
+import express from "express";
+import mongoose from "mongoose";
+import compression from "compression";
+import helmet from "helmet";
+import path from "path";
+import endpoints from "./endpoints.config";
 
-const app: Express = express()
-const port = process.env.PORT || 3000
+const port = endpoints.PORT || 5000;
+
+// Connect to MongoDB
+mongoose.connect(endpoints.MONGO_URL);
+
+const app: express.Express = express();
+
+app.use(compression()); // compress all paths
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  })
+);
+
+//Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// view engine setup
+app.set("views", path.join(__dirname, "..", "views"))
+app.set("view engine", "ejs")
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
-})
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
