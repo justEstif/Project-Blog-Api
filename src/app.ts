dff3769cf8a5810b4973743ee7077060d13df137
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-29 06:07:52 -0400

    🎉 Initial npm packages and dir structure

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Express } from "express"
+
+const app: Express = express()
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
+})
