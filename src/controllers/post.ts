8b50c44a8243802fcec586da4231af893d29b1bd
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-31 10:59:35 -0400

    feat: add update post request handler

--- src/controllers/post.ts
+++ src/controllers/post.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from 'express'
-import asyncHandler from 'express-async-handler'
-import { body, validationResult } from 'express-validator'
+import { Types } from 'mongoose'
+import { Request, Response, RequestHandler } from 'express'
+import { body, validationResult, param } from 'express-validator'
 import { Post } from '../models/'
 
 // @desc Return posts in desc order by publication_date
@@ -22,7 +22,7 @@ export const getPosts: RequestHandler = (_, res) => {
 // @route POST /api/posts
 // @access Private
 export const setPosts = [
-  // TODO: Handle post set/create
+  // TODO: BUG Check that the owner is making the post.
   body('title', 'Title must not be empty.')
     .trim()
     .escape()
@@ -68,7 +68,7 @@ export const setPosts = [
       default:
         post.save((err) => {
           if (err) {
-            res.json(400).json({
+            res.status(400).json({
               message: 'Error when saving post',
               errors: err,
               post: post
@@ -87,15 +87,75 @@ export const setPosts = [
 // @desc Update a post
 // @route PUT /api/posts/:id
 // @access Private
-export const updatePost = asyncHandler(async (req, res) => {
-  // TODO: Handle post update
-  res.status(200).json({ message: `PUT: update post ${req.params.id}` })
-})
+export const updatePost = [
+  body('title', 'Title must not be empty.')
+    .trim()
+    .escape()
+    .isLength({ min: 3 })
+    .withMessage('The title must be at least 3 characters long.'),
+  body('body', 'Body must not be empty.')
+    .trim()
+    .escape()
+    .isLength({ min: 10 })
+    .withMessage('The body must be at least 10 characters long.'),
+  body('summary', 'Summary must not be empty.')
+    .trim()
+    .escape()
+    .isLength({ min: 5 })
+    .withMessage('The summary must be at least 5 characters long.'),
+  body('tags')
+    .isArray({ min: 1 })
+    .withMessage('At least one tag must be passed'),
+  body('tags.*')
+    .trim()
+    .escape()
+    .isString()
+    .withMessage('Every tag must be a valid string')
+    .isLength({ min: 3 })
+    .withMessage('Every tag must be at least 3 characters long'),
+  param('id').customSanitizer((value) => new Types.ObjectId(value)),
+
+  (req: Request, res: Response) => {
+    const post = new Post({
+      title: req.body.title,
+      body: req.body.body,
+      summary: req.body.summary,
+      tags: req.body.tags,
+      _id: req.params.id
+    })
+
+    const errors = validationResult(req)
+    switch (!errors.isEmpty()) {
+      case true:
+        res.status(401).json({
+          message: 'Failed to save post',
+          errors: errors,
+          post: post
+        })
+        return
+      default:
+        Post.findByIdAndUpdate(req.params.id, post, { new: true }, (err) => {
+          if (err) {
+            res.status(400).json({
+              message: 'Error when saving post',
+              errors: err,
+              post: post
+            })
+          } else {
+            res.status(200).json({
+              message: 'Post updated',
+              post: post
+            })
+          }
+        })
+    }
+  }
+]
 
 // @desc Delete a post
 // @route DELETE /api/posts/:id
 // @access Private
-export const deletePost = asyncHandler(async (req, res) => {
+export const deletePost: RequestHandler = (req, res) => {
   // TODO: Handle post delete
   res.status(200).json({ message: `Delete: delete post ${req.params.id}` })
-})
+}
