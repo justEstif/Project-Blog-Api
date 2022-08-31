d1fa6b2aff101d49511bced7336efed463bfa476
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-31 10:11:40 -0400

    fix: change path to include .env

--- src/config/endpoints.ts
+++ src/config/endpoints.ts
@@ -1,10 +1,9 @@
-import * as dotenv from "dotenv"
-import path from "path"
+import * as dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config({ path: path.resolve(__dirname, "..", ".env") })
+dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') })
 
 export default {
-  MONGO_URL: process.env.MONGO_URL ?? "",
-  PORT: process.env.PORT || 5000,
+  MONGO_URL: process.env.MONGO_URL ?? '',
+  PORT: process.env.PORT || 5000
 }
-
