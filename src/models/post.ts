6dfe4163b1b63f7eeacbce825e9820f3fa2377fa
justEstif <95101485+justEstif@users.noreply.github.com>
2022-08-31 10:09:51 -0400

    feat(post, model): add mongoose validation

--- src/models/post.ts
+++ src/models/post.ts
@@ -11,26 +11,56 @@ interface IPost {
 }
 
 const PostSchema = new Schema<IPost>({
-  title: { type: String, minlength: 3, required: true },
-  body: { type: String, minlength: 10, required: true },
-  summary: { type: String, minlength: 5, required: true },
-  tags: [{ type: String, required: true, minlength: 3 }], // at least one tag req
+  title: {
+    type: String,
+    minlength: [3, 'The title must be at least 3 characters long.'],
+    required: true
+  },
+  body: {
+    type: String,
+    minlength: [10, 'The body must be at least 10 characters long.'],
+    required: true
+  },
+  summary: {
+    type: String,
+    minlength: [5, 'The summary must be at least 5 characters long.'],
+    required: true
+  },
+  tags: {
+    type: [String],
+    required: true,
+    validate: [
+      {
+        validator: (tagArray: string[]) => tagArray.length >= 0 || !tagArray,
+        message: 'At least one tag must be passed.'
+      },
+      {
+        validator: (array: string[]) => array.every((tag) => tag.length >= 3),
+        message: 'Every tag must be at least 3 characters.'
+      },
+      {
+        validator: (array: string[]) =>
+          array.every((tag) => typeof tag === 'string'),
+        message: 'Every tag must be a valid string'
+      }
+    ]
+  }, // at least one tag req
   published: { type: Boolean, default: false },
-  publication_date: { type: Date, default: undefined },
-  comment_ids: [{ type: Types.ObjectId, ref: 'User' }] // default empty
+  publicationDate: { type: Date, default: undefined },
+  commentIds: [{ type: Types.ObjectId, ref: 'User' }] // default empty
 })
 
 // Add date when published
 PostSchema.pre('save', function (next) {
   const post = this
   // if published and undefined(not to overwrite it)
-  if (post.published && !post.publication_date) {
-    post.publication_date = new Date()
+  if (post.published && !post.publicationDate) {
+    post.publicationDate = new Date()
   }
   next()
 })
 
 // TODO: Virtuals
-const Post = model<IPost>('Comment', PostSchema)
+const Post = model<IPost>('Post', PostSchema)
 export { IPost }
 export default Post
