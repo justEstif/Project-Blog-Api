03717119bd3e3918e4cb6b6821cd49d31b062744
Estifanos <estifanosbeyene21@gmail.com>
2022-09-03 18:19:17 -0400

    feat: remove skip properties option

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -4,12 +4,9 @@ import { validate, ValidationError } from 'class-validator'
 import { RequestHandler } from 'express'
 import HttpException from '../exceptions/HttpException'
 
-function validationMiddleware(
-  type: any,
-  skipMissingProperties = false
-): RequestHandler {
+function validationMiddleware(type: any): RequestHandler {
   return (req, _, next) => {
-    validate(plainToInstance(type, req.body), { skipMissingProperties }).then(
+    validate(plainToInstance(type, req.body)).then(
       (errors: ValidationError[]) => {
         if (errors.length > 0) {
           const message = errors
