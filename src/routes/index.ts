06e2adff5d5cbc6411f45d63135e24d03aa13903
Estifanos <estifanosbeyene21@gmail.com>
2022-09-03 12:13:51 -0400

    refactor(routes): remove unused routes file

--- src/routes/index.ts
+++
@@ -1,4 +0,0 @@
-import post_routes from './post'
-import user_routes from './user'
-
-export { post_routes, user_routes }
--- src/routes/post.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { getPosts, setPosts, deletePost, updatePost } from '../controllers/post'
-
-const router = Router()
-router.route('/').get(getPosts).post(setPosts)
-router.route('/:id').put(updatePost).delete(deletePost)
-
-export default router
