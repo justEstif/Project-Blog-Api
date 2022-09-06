576579bb423432f09f557bb3376af8b74a87e5a7
Estifanos <estifanosbeyene21@gmail.com>
2022-09-06 06:02:06 -0400

    feat: add auth middleware and auth errors

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken'
+import endpoints from '../utils/endpoints'
+import AuthenticationTokenMissingException from '../exception/AuthenticationTokenMissingException'
+import WrongAuthenticationTokenException from '../exception/WrongAuthenticationTokenMissingException'
+import DataStoredInToken from '../interface/dataStoredInToken'
+import asyncHander from 'express-async-handler'
+import User from '../user/user.model'
+
+const authMiddleware = asyncHander(async (request, _, next) => {
+  const cookies = request.cookies
+  if (cookies && cookies.Authorization) {
+    const secret = endpoints.JWT_SECRET
+
+    try {
+      const { _id: id } = jwt.verify(
+        cookies.Authorization,
+        secret
+      ) as DataStoredInToken
+      const user = await User.findById(id)
+      if (user) {
+        request.user = user
+        next()
+      } else {
+        next(new WrongAuthenticationTokenException())
+      }
+    } catch (error) {
+      next(new WrongAuthenticationTokenException())
+    }
+  } else {
+    next(new AuthenticationTokenMissingException())
+  }
+})
+
+export default authMiddleware
