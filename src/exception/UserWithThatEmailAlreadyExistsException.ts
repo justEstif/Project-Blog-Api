7610d384434f16f4b052773fd78374615464b9f6
Estifanos <estifanosbeyene21@gmail.com>
2022-09-06 10:03:44 -0400

    feat: change exception to be used for email and username

--- src/exception/UserWithThatEmailAlreadyExistsException.ts
+++
@@ -1,9 +0,0 @@
-import HttpException from '../exception/HttpException';
-
-class UserWithThatEmailAlreadyExistsException extends HttpException {
-  constructor(email: string) {
-    super(400, `User with email ${email} already exists`);
-  }
-}
-
-export default UserWithThatEmailAlreadyExistsException;
