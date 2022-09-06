92956f1a2b180d59cdc6dd53173bf7169c30e7d4
Estifanos <estifanosbeyene21@gmail.com>
2022-09-06 10:21:46 -0400

    feat: add owner middleware to post requests

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -7,41 +7,37 @@ import HttpException from '../exception/HttpException'
 import PostNotFoundException from '../exception/PostNotFoundException'
 import validationMiddleware from '../middleware/validation.middleware'
 import CreatePostDto from './post.dto'
-import authMiddleware from 'src/middleware/auth.middleware'
+import authMiddleware from '../middleware/auth.middleware'
+import ownerMiddleware from '../middleware/owner.middleware'
 
 class PostController implements IController {
   public path = '/api/posts'
   public path_all = `${this.path}/*`
   public path_id = `${this.path}/:id`
   public router = Router()
-  public postModel = PostModel
 
   constructor() {
     this.intializeRoutes()
   }
 
   public intializeRoutes() {
+    // public routes
     this.router.get(this.path, this.getPosts)
     this.router.get(this.path_id, this.getPostById)
-    this.router.post(
-      this.path,
-      validationMiddleware(CreatePostDto),
-      this.createPost
-    )
-    this.router.put(
-      this.path_id,
-      validationMiddleware(CreatePostDto),
-      this.updatePost
-    )
-    this.router.delete(this.path_id, this.deletePost)
-
+    // private routes -> only owner
     this.router
-      .all(this.path_all, authMiddleware)
-      .put(this.path_id, validationMiddleware(CreatePostDto), this.updatePost)
-      .delete(this.path_id, this.deletePost)
+      .put(
+        this.path_id,
+        authMiddleware,
+        ownerMiddleware,
+        validationMiddleware(CreatePostDto),
+        this.updatePost
+      )
+      .delete(this.path_id, authMiddleware, ownerMiddleware, this.deletePost)
       .post(
         this.path,
         authMiddleware,
+        ownerMiddleware,
         validationMiddleware(CreatePostDto),
         this.createPost
       )
@@ -51,8 +47,7 @@ class PostController implements IController {
   // @route GET /api/posts
   // @access Public
   private getPosts(_: Request, response: Response, next: NextFunction) {
-    this.postModel
-      .find()
+    PostModel.find()
       .sort({ publication_date: 1 })
       .exec((error, posts) => {
         if (error) {
@@ -75,7 +70,7 @@ class PostController implements IController {
     next: NextFunction
   ) {
     const id = new Types.ObjectId(request.params.id)
-    this.postModel.findById(id).exec((error, post) => {
+    PostModel.findById(id).exec((error, post) => {
       if (error) {
         next(new PostNotFoundException(id))
       } else {
@@ -92,7 +87,7 @@ class PostController implements IController {
   // @access Private
   private createPost(request: Request, response: Response, next: NextFunction) {
     const postData: IPost = request.body
-    this.postModel.create(postData, (error, post) => {
+    PostModel.create(postData, (error, post) => {
       if (error) {
         next(new HttpException(404, 'Failed to create post'))
       } else {
@@ -110,7 +105,7 @@ class PostController implements IController {
   private updatePost(request: Request, response: Response, next: NextFunction) {
     const id = new Types.ObjectId(request.params.id)
     const post: IPost = request.body
-    this.postModel.findByIdAndUpdate(id, post, { new: true }).exec((error) => {
+    PostModel.findByIdAndUpdate(id, post, { new: true }).exec((error) => {
       if (error) {
         next(new PostNotFoundException(id))
       } else {
@@ -127,7 +122,7 @@ class PostController implements IController {
   // @access Private
   private deletePost(request: Request, response: Response, next: NextFunction) {
     const id = new Types.ObjectId(request.params.id)
-    this.postModel.findByIdAndRemove(id).exec((error) => {
+    PostModel.findByIdAndRemove(id).exec((error) => {
       if (error) {
         next(new PostNotFoundException(id))
       } else {
