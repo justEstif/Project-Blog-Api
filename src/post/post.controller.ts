fbbcf86678d49e6e83abcf814d6f108ecf7923a1
Estifanos <estifanosbeyene21@gmail.com>
2022-09-07 05:22:42 -0400

    feat: add create and get comment functions

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -9,11 +9,14 @@ import validationMiddleware from '../middleware/validation.middleware'
 import CreatePostDto from './post.dto'
 import authMiddleware from '../middleware/auth.middleware'
 import ownerMiddleware from '../middleware/owner.middleware'
+import CommentDto from '../comment/comment.dto'
+import Comment from '../comment/comment.model'
 
 class PostController implements IController {
   public path = '/api/posts'
   public path_all = `${this.path}/*`
   public path_id = `${this.path}/:id`
+  public path_id_comment = `${this.path}/:id/comment`
   public router = Router()
 
   constructor() {
@@ -41,26 +44,36 @@ class PostController implements IController {
         validationMiddleware(CreatePostDto),
         this.createPost
       )
+      .post(
+        this.path_id_comment,
+        authMiddleware,
+        validationMiddleware(CommentDto),
+        this.createComment
+      )
   }
 
+  // TODO: Don't get posts that aren't published if not owner
   // @desc Return posts in desc order by publication_date
   // @route GET /api/posts
   // @access Public
   private getPosts(_: Request, response: Response, next: NextFunction) {
+    // if owner -> get all
+    // else get published only
     PostModel.find()
-      .sort({ publication_date: 1 })
+      .sort({ publicationDate: 1 })
       .exec((error, posts) => {
         if (error) {
           next(new HttpException(404, 'Failed to get posts'))
         } else {
           response.status(200).json({
-            Message: 'Posts received',
-            Posts: posts
+            message: 'Posts received',
+            posts: posts
           })
         }
       })
   }
 
+  // TODO: Don't get posts that isn't published if not owner
   // @desc Return post from the id
   // @route GET /api/posts:id
   // @access Public
@@ -70,14 +83,26 @@ class PostController implements IController {
     next: NextFunction
   ) {
     const id = new Types.ObjectId(request.params.id)
+    // if post is published and user is owner show
+    // else don't
     PostModel.findById(id).exec((error, post) => {
       if (error) {
         next(new PostNotFoundException(id))
       } else {
-        response.status(200).json({
-          Message: 'Post received',
-          Post: post
-        })
+        Comment.find({ postId: id }) // TODO: Move this function somewhere else
+          .sort({ commentDate: 1 })
+          .populate('user', 'username') // NOTE: Get the username only
+          .exec((error, comments) => {
+            if (error) {
+              next(new PostNotFoundException(id))
+            } else {
+              response.status(200).json({
+                message: 'Post received',
+                post: post,
+                comments: comments
+              })
+            }
+          })
       }
     })
   }
@@ -92,8 +117,8 @@ class PostController implements IController {
         next(new HttpException(404, 'Failed to create post'))
       } else {
         response.status(200).json({
-          Message: 'Post saved',
-          Post: post
+          message: 'Post saved',
+          post: post
         })
       }
     })
@@ -110,8 +135,8 @@ class PostController implements IController {
         next(new PostNotFoundException(id))
       } else {
         response.status(200).json({
-          Message: 'Post updated',
-          Post: post
+          message: 'Post updated',
+          post: post
         })
       }
     })
@@ -127,8 +152,34 @@ class PostController implements IController {
         next(new PostNotFoundException(id))
       } else {
         response.status(200).json({
-          Message: 'Post deleted',
-          Post: id
+          message: 'Post deleted',
+          post: id
+        })
+      }
+    })
+  }
+
+  // @desc Create a comment
+  // @route POST /api/posts/:id/comment
+  // @access Private
+  private createComment(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    const commentData: CommentDto = request.body
+    const newComment = {
+      ...commentData,
+      user: request.user._id, // current user id
+      postId: request.params.id // current post id
+    }
+    Comment.create(newComment, (error, comment) => {
+      if (error) {
+        next(new HttpException(404, 'Failed to create comment'))
+      } else {
+        response.status(200).json({
+          message: 'Comment saved',
+          comment: comment
         })
       }
     })
