7219bfdf1212c2f9ea5b5ab78ec13336fff6ab54
Estifanos <estifanosbeyene21@gmail.com>
2022-09-14 15:30:49 -0400

    feat: remove unused login function and exception

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,6 +1,5 @@
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import WrongCredentialsException from '../exception/WrongCredentialsException'
 import UserAlreadyExistsException from '../exception/UserAlreadyExistsException'
 import DataStoredInToken from '../interface/dataStoredInToken'
 import TokenData from '../interface/tokenData.interface'
@@ -8,7 +7,6 @@ import CreateUserDto from '../user/user.dto'
 import IUser from '../user/user.interface'
 import UserModel from '../user/user.model'
 import endpoints from '../utils/endpoints'
-import LogInDto from './logIn.dto'
 
 class AuthenticationService {
   public register = async (userData: CreateUserDto) => {
@@ -28,41 +26,12 @@ class AuthenticationService {
         password: hashedPassword // adding hashed pw to db
       })
       user.password = undefined // clearing user pw from response
-      const tokenData = this.createToken(user)
-      const cookie = this.createCookie(tokenData)
-      return { user, cookie }
+      const token = this.createToken(user)
+      return { user, token }
     }
   }
 
-  public logIn = async (logInData: LogInDto) => {
-    const user = await UserModel.findOne({ email: logInData.email })
-    if (user) {
-      const matchingPassword = await bcrypt.compare(
-        logInData.password,
-        user.get('password', null, { getters: false })
-      )
-      if (matchingPassword) {
-        const tokenData = this.createToken(user)
-        const cookie = this.createCookie(tokenData)
-        user.password = undefined // clearing user pw from response
-        return { user, cookie }
-      } else {
-        throw new WrongCredentialsException()
-      }
-    } else {
-      throw new WrongCredentialsException()
-    }
-  }
-
-  public logOut = () => {
-    return 'Authorization=;Max-age=0'
-  }
-
-  private createCookie(tokenData: TokenData) {
-    return `Authorization=${tokenData.token}; HttpOnly; Max-Age=${tokenData.expiresIn}`
-  }
-
-  private createToken(user: IUser): TokenData {
+  public createToken(user: IUser): TokenData {
     const expiresIn = 24 * 60 * 60 // a day
     const secret = endpoints.JWT_SECRET
     const dataStoredInToken: DataStoredInToken = {
