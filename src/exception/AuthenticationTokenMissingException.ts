3f3b6d21baea1149a83654f99a96959c1baf0ae4
Estifanos <estifanosbeyene21@gmail.com>
2022-09-14 15:31:31 -0400

    feat: change ownerMiddleware to use authMiddleware

--- src/exception/AuthenticationTokenMissingException.ts
+++ src/exception/AuthenticationTokenMissingException.ts
@@ -1,5 +1,6 @@
 import HttpException from './HttpException'
 
+// NOTE: THIS ISN'T USED ANYWHERE
 class AuthenticationTokenMissingException extends HttpException {
   constructor() {
     super(401, 'Authentication token missing')
--- src/middleware/owner.middleware.ts
+++ src/middleware/owner.middleware.ts
@@ -1,13 +1,17 @@
 import { RequestHandler } from 'express'
+import authMiddleware from './auth.middleware'
 import NotOwnerException from '../exception/NotOwnerException'
 
+// Unnecessary anymore
 // Only owner is allowed on this route
-const ownerMiddleware: RequestHandler = (req, _, next) => {
-  if (req.user.owner) {
-    next()
-  } else {
-    next(new NotOwnerException())
-  }
+const ownerMiddleware: RequestHandler = (request, response, next) => {
+  authMiddleware(request, response, () => {
+    if (request.user.owner) {
+      next()
+    } else {
+      next(new NotOwnerException())
+    }
+  })
 }
 
 export default ownerMiddleware
