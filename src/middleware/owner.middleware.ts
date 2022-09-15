7db9903ddf3fa9ad49130f14e77c7005f2c20e71
Estifanos <estifanosbeyene21@gmail.com>
2022-09-15 06:07:46 -0400

    feat: check error to json response for testing

--- src/middleware/owner.middleware.ts
+++ src/middleware/owner.middleware.ts
@@ -1,15 +1,16 @@
 import { RequestHandler } from 'express'
 import authMiddleware from './auth.middleware'
-import NotOwnerException from '../exception/NotOwnerException'
+// import NotOwnerException from '../exception/NotOwnerException'
 
-// Unnecessary anymore
 // Only owner is allowed on this route
 const ownerMiddleware: RequestHandler = (request, response, next) => {
   authMiddleware(request, response, () => {
     if (request.user.owner) {
       next()
     } else {
-      next(new NotOwnerException())
+      response.status(400).json({
+        message: 'Not owner: Route not allowed'
+      })
     }
   })
 }
