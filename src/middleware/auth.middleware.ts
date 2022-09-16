2a548285e4c9c0c4cb4d81702b2f5d7b99330d82
Estifanos <estifanosbeyene21@gmail.com>
2022-09-16 02:52:20 -0400

    feat(auth.middleware): add error message

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,13 +1,11 @@
 import { RequestHandler } from 'express'
 import passport from 'passport'
+import WrongAuthenticationTokenException from 'src/exception/WrongAuthenticationTokenMissingException'
 
 const authMiddleware: RequestHandler = (request, response, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err || !user) {
-      // TODO: Add error here
-      response.status(400).json({
-        message: 'Something is not right'
-      })
+      next(new WrongAuthenticationTokenException())
     } else {
       request.user = user // access the user in this route
       next()
