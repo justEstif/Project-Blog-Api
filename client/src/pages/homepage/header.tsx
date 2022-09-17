50dd6847812e18beb5985f8d49e5173b0ddff580
Estifanos <estifanosbeyene21@gmail.com>
2022-09-17 08:54:49 -0400

    style: improve header styling to match model

--- client/src/pages/homepage/header.tsx
+++ client/src/pages/homepage/header.tsx
@@ -1,17 +1,22 @@
-// TODO: Add login, logout, register buttons
+import tw from 'tailwind-styled-components'
+
 const Header = () => {
+  const SHeader = tw.header`
+    flex
+    flex-col
+    gap-8
+    text-center
+    tracking-wider
+    leading-5
+    mb-10
+  `
   return (
-    <header>
-      <ul className="flex flex-col gap-8 tracking-wider leading-5">
-        <li className="text-5xl text-center">Interesting Blog</li>
-        <li className="text-center uppercase text-md">Estifanos Beyene</li>
-        <li className="text-center text-md">Abstract</li>
-        <li className="text-center text-md">
-          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
-          cillum sint consectetur cupidatat.
-        </li>
-      </ul>
-    </header>
+    <SHeader>
+      <p className="text-5xl capitalize">Interesting Blog</p>
+      <p className="uppercase text-md">Estifanos Beyene</p>
+      <p className="capitalize text-md">Abstract</p>
+      <p className="text-md">I am a fullstack developer.</p>
+    </SHeader>
   )
 }
 
