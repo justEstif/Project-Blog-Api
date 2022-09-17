d9403a8f20f13f1891e388dae140021bd9eb0df4
Estifanos <estifanosbeyene21@gmail.com>
2022-09-17 14:48:35 -0400

    feat: create header for homepage

---
+++ client/src/pages/homepage/Header.tsx
@@ -0,0 +1,25 @@
+import tw from 'tailwind-styled-components'
+
+const Header = () => {
+  const SHeader = tw.header`
+    flex
+    flex-col
+    gap-8
+    text-center
+    tracking-wider
+    leading-5
+    mb-10
+  `
+  return (
+    <>
+      <SHeader>
+        <p className="text-5xl capitalize">Interesting Blog</p>
+        <p className="uppercase text-md">Estifanos Beyene</p>
+        <p className="capitalize text-md">Abstract</p>
+        <p className="text-md">I am a fullstack developer.</p>
+      </SHeader>
+    </>
+  )
+}
+
+export default Header
