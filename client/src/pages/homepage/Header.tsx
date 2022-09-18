0264191bb27da669372ddcad760ba6e3b5d9a4ec
Estifanos <estifanosbeyene21@gmail.com>
2022-09-18 05:03:10 -0400

    feat: move header style to separate componet

--- client/src/pages/homepage/Header.tsx
+++ client/src/pages/homepage/Header.tsx
@@ -1,15 +1,6 @@
-import tw from 'tailwind-styled-components'
+import SHeader from '../../components/SHeader'
 
 const Header = () => {
-  const SHeader = tw.header`
-    flex
-    flex-col
-    gap-8
-    text-center
-    tracking-wider
-    leading-5
-    mb-10
-  `
   return (
     <SHeader>
       <p className="text-5xl capitalize">Interesting Blog</p>
