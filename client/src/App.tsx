d859e81ce15ea5a633112e35366db01a473097a9
Estifanos <estifanosbeyene21@gmail.com>
2022-09-18 04:33:35 -0400

    feat: move get posts from app to homepage index

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,8 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import HomePage from './pages/homepage'
-import Layout from './components/Layout'
-import IPost from './interface/IPost'
-import NavBar from './components/NavBar'
-
 function App() {
-  const [posts, setPosts] = useState<IPost[]>([]) // TODO: add interface
-  const urlwithProxy = '/api/posts'
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(urlwithProxy)
-        const data: IPost[] = response.data
-        setPosts(() => data)
-      } catch (error) {
-        console.log(error)
-      }
-    }
-    fetchData()
-  }, [])
-
   return (
-    <Layout>
-      <NavBar />
-      <HomePage postsProp={posts} />
-    </Layout>
+    <div>
+      <h1>This is App</h1>
+    </div>
   )
 }
 
--- client/src/pages/homepage/index.tsx
+++ client/src/pages/homepage/index.tsx
@@ -1,24 +1,37 @@
-// TODO: Add login, logout, register buttons
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import IPost from '../../interface/IPost'
 import Header from './Header'
 import Post from './Post'
 import { nanoid } from 'nanoid'
 
-interface Props {
-  postsProp: IPost[]
-}
+interface Props {}
 
-const HomePage = ({ postsProp }: Props) => {
-  const posts =
-    postsProp.length === 0 ? (
-      <p>loading</p>
-    ) : (
-      postsProp.map((post) => <Post key={nanoid()} post={post} />)
-    )
+const HomePage = ({}: Props) => {
+  const [posts, setPosts] = useState<IPost[]>([]) // TODO: add interface
+  const urlwithProxy = '/api/posts'
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(urlwithProxy)
+        const data: IPost[] = response.data
+        setPosts(() => data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [])
+
   return (
     <section>
       <Header />
-      {posts}
+      {posts.length === 0 ? (
+        <p>loading</p>
+      ) : (
+        posts.map((post) => <Post key={nanoid()} post={post} />)
+      )}
     </section>
   )
 }
