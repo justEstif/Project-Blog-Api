c5f8178eed6dc56477377a43d0c564df1553be88
Estifanos <estifanosbeyene21@gmail.com>
2022-09-19 02:56:30 -0400

    feat: store user credential in state

--- client/src/pages/loginpage/index.tsx
+++ client/src/pages/loginpage/index.tsx
@@ -1,18 +1,27 @@
 // import tw from 'tailwind-styled-components'
+import { useEffect, useState } from 'react'
 import SHeader from '../../components/SHeader'
 import Form from './Form'
+import { IUserCredentials } from './interface'
 interface Props {}
 
 const LoginPage = ({}: Props) => {
+  const [userCredentials, setUserCredentials] = useState<IUserCredentials>({
+    email: '',
+    password: ''
+  })
+
+  useEffect(() => {
+    console.log(userCredentials) // send a post request for jest
+  }, [userCredentials])
+
   return (
     <>
       <SHeader>
         <p className="text-5xl capitalize">Login</p>
       </SHeader>
 
-      <div>
-        <Form />
-      </div>
+      <Form setUserCredentials={setUserCredentials} />
     </>
   )
 }
