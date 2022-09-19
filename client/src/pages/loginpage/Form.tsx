7d4e4693151650cfcec6aec61fccd333cf43097f
Estifanos <estifanosbeyene21@gmail.com>
2022-09-19 02:56:12 -0400

    feat: improve form

--- client/src/pages/loginpage/Form.tsx
+++ client/src/pages/loginpage/Form.tsx
@@ -1,24 +1,55 @@
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-interface Inputs {
-  email: string
-  password: string
+import tw from 'tailwind-styled-components'
+import { IUserCredentials } from './interface'
+import { Dispatch, SetStateAction } from 'react'
+
+interface Props {
+  setUserCredentials: Dispatch<SetStateAction<IUserCredentials>>
 }
 
-const Form = () => {
-  const { register, handleSubmit } = useForm<Inputs>()
+const Form = ({ setUserCredentials }: Props) => {
+  const { register, handleSubmit } = useForm<IUserCredentials>()
+  const SInput = tw.input`
+    max-w-xs
+    appearance-none
+    bg-gray-200
+    border-2
+    border-gray-200
+    rounded
+    py-2
+    px-4
+    text-gray-700
+    leading-tight
+    focus:outline-none
+    focus:bg-white
+    focus:border-purple-500
+  `
+
+  const SButton = tw.button`
+    shadow
+    bg-purple-500
+    hover:bg-purple-400
+    focus:shadow-outline
+    focus:outline-none
+    text-white
+    font-bold
+    py-2
+    px-4
+    rounded
+  `
 
   return (
-    <>
+    <div className="flex flex-col justify-center content-center">
       <form
         className="flex flex-col gap-6 mx-auto max-w-md"
-        onSubmit={handleSubmit((data) => console.log(data))}
+        onSubmit={handleSubmit((data) => setUserCredentials(data))}
       >
         <div className="flex flex-col gap-3">
           <label htmlFor="email" className="font-mono font-bold text-gray-500">
             Email
           </label>
-          <input
+          <SInput
             autoComplete="off"
             id="email"
             type="email"
@@ -33,28 +64,27 @@ const Form = () => {
           >
             Password
           </label>
-          <input
+          <SInput
             autoComplete="off"
             id="password"
-            className="w-full max-w-xs input input-bordered"
             type="password"
-            placeholder="Enter password..."
+            placeholder="******************"
             {...register('password')}
           />
         </div>
-        <div className="flex gap-6">
-          <button
-            type="submit"
-            className="hover:border-b-2 hover:border-gray-800"
-          >
-            Login
-          </button>
-          <button className="italic hover:border-b-2 hover:border-gray-800">
-            <Link to="/register">Register User</Link>
-          </button>
+        <div className="flex flex-col gap-6">
+          <div className="flex justify-center">
+            <button>
+              New to blog?
+              <span className="italic border-b-2 border-red-800">
+                <Link to="/register"> Register User</Link>
+              </span>
+            </button>
+          </div>
+          <SButton type="submit">Login</SButton>
         </div>
       </form>
-    </>
+    </div>
   )
 }
 
