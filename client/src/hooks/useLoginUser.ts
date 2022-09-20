f831fbb5c69e4e742749aa871a94c6e617116090
Estifanos <estifanosbeyene21@gmail.com>
2022-09-20 15:24:08 -0400

    feat(useLoginUser): custom hook for login user

---
+++ client/src/hooks/useLoginUser.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import useStore from '../store'
+import IUserCredentials from '../interface/IUserCredentials'
+import { loginUser } from '../services/api.auth'
+
+const useLoginUser = () => {
+  const store = useStore((state) => state)
+  const [message, setMessage] = useState('')
+  const [userCredentials, setUserCredentials] = useState<IUserCredentials>({
+    email: '',
+    password: ''
+  })
+
+  useEffect(() => {
+    const handleLogin = async () => {
+      const response = await loginUser(userCredentials)
+      if (typeof response === 'string') {
+        setMessage(message)
+      } else {
+        store.loginUser(response)
+      }
+    }
+    if (!(userCredentials.email === '' || userCredentials.password === '')) {
+      handleLogin()
+    }
+  }, [userCredentials])
+
+  return {
+    setUserCredentials,
+    message
+  }
+}
+
+export default useLoginUser
--- client/src/pages/loginpage/index.tsx
+++ client/src/pages/loginpage/index.tsx
@@ -4,30 +4,12 @@ import { loginUser } from '../../services/api.auth'
 import Form from './Form'
 import IUserCredentials from '../../interface/IUserCredentials'
 import useStore from '../../store'
+import useLoginUser from '../../hooks/useLoginUser'
 
-interface Props {}
+interface Props { }
 
-const LoginPage = ({}: Props) => {
-  const store = useStore((state) => state)
-  const [message, setMessage] = useState('')
-  const [userCredentials, setUserCredentials] = useState<IUserCredentials>({
-    email: '',
-    password: ''
-  })
-
-  useEffect(() => {
-    const handleLogin = async () => {
-      const response = await loginUser(userCredentials)
-      if (typeof response === 'string') {
-        setMessage(message)
-      } else {
-        store.loginUser(response)
-      }
-    }
-    if (!(userCredentials.email === '' || userCredentials.password === '')) {
-      handleLogin()
-    }
-  }, [userCredentials])
+const LoginPage = ({ }: Props) => {
+  const { message, setUserCredentials } = useLoginUser()
 
   return (
     <>
