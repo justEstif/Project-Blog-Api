a568a8b9d905c2084ca31d27dcc7a3b142b26c5b
Estifanos <estifanosbeyene21@gmail.com>
2022-09-20 04:16:31 -0400

    feat: add a set user function to update the user

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,13 +1,16 @@
 import create from 'zustand'
 import IGlobal from '../interface/IGlobal'
 import IUser from '../interface/IUser'
-import { loginUser } from '../services/api.auth'
 
 const useStore = create<IGlobal>((set) => ({
   user: null,
-  loginUser: loginUser,
+  setUser: (user: IUser | null) =>
+    set((state) => ({
+      ...state,
+      user: user
+    }))
+
   // NOTE: to log out just set the user to null
-  setUser: (user: IUser | null) => set((states) => ({ ...states, user }))
 }))
 
 export default useStore
