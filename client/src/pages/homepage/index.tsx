c2dc20e407453b3d1ace6a610fcdc2287c527859
Estifanos <estifanosbeyene21@gmail.com>
2022-09-20 09:54:07 -0400

    feat: include current user token in the getPosts

--- client/src/pages/homepage/index.tsx
+++ client/src/pages/homepage/index.tsx
@@ -4,16 +4,20 @@ import Header from './Header'
 import Post from './Post'
 import { nanoid } from 'nanoid'
 import { getPosts } from '../../services/api'
+import useStore from '../../store'
 
 interface Props { }
 
 const HomePage = ({ }: Props) => {
   const [posts, setPosts] = useState<IPost[]>([]) // TODO: add interface
+  const store = useStore((state) => state)
 
   useEffect(() => {
     const fetchData = async () => {
+    const token = store.user?.token.token || ''
+    console.log(`Token: ${token}`)
       try {
-        const data = await getPosts()
+        const data = await getPosts(token)
         setPosts(() => data)
       } catch (error) {
         // TODO: Better error handling
--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -3,10 +3,15 @@ import IPost from '../interface/IPost'
 
 // NOTE: Name the functions same as on the backend
 // TODO: Check if there is an active user
-export const getPosts = async (): Promise<IPost[] | []> => {
+export const getPosts = async (token: string): Promise<IPost[] | []> => {
   const urlwithProxy = '/api/posts'
   try {
-    const response = await axios.get(urlwithProxy)
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+    const response = await axios.get(urlwithProxy, config)
     const data: IPost[] = response.data
     return data
   } catch (error) {
