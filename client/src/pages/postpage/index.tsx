41fd38338c52befee9212e74b0ee9a8e4d08abdf
Estifanos <estifanosbeyene21@gmail.com>
2022-09-21 15:07:44 -0400

    chore: remove unused code

--- client/src/pages/postpage/index.tsx
+++ client/src/pages/postpage/index.tsx
@@ -1,22 +1,23 @@
+import SHeader from '../../components/SHeader'
 import { useLocation } from 'react-router-dom'
 import useGetPost from '../../hooks/useGetPost'
 
 const PostPage = () => {
   const postID = useLocation().state
   const post = useGetPost(postID)
-  console.log(post)
 
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     try {
-  //       const data = await getPost(location.state)
-  //       setPosts(() => data)
-  //     }
-  //     }
-  //
-  // }, [params.id])
   // TODO: call the api and get post info
-  return <div>Hello from post page: </div>
+  return (
+    <>
+      <SHeader>
+        {typeof post === 'undefined' ? (
+          <p> Loading</p>
+        ) : (
+          <p className="text-5xl capitalize">{post?.title}</p>
+        )}
+      </SHeader>
+    </>
+  )
 }
 
 export default PostPage
