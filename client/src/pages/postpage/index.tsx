d462ecbce7a9f324fe744ddfbe1f966789d4aa3e
Estifanos <estifanosbeyene21@gmail.com>
2022-09-22 10:40:10 -0400

    feat(PostPage): add comment info

--- client/src/pages/postpage/index.tsx
+++ client/src/pages/postpage/index.tsx
@@ -2,17 +2,28 @@ import { nanoid } from 'nanoid'
 import SHeader from '../../components/SHeader'
 import { useLocation } from 'react-router-dom'
 import useGetPost from '../../hooks/useGetPost'
+import getFormattedDate from '../../utils/getFormattedDate'
 
 const PostPage = () => {
   const postID = useLocation().state
-  const { post, comment } = useGetPost(postID)
+  const { post, comments } = useGetPost(postID)
   const postPubDate = post?.publicationDate?.toString() ?? 'Not Published'
+
+  const commentss = comments?.map((comment) => {
+    const commentDate = getFormattedDate(new Date(comment.commentDate))
+    const user = comment.user || null
+    return (
+      <p key={nanoid()}>
+        {user ? user.username : 'username '}
+        {comment.body} {commentDate}
+      </p>
+    )
+  })
   const tags = post?.tags.map((tag) => (
     <p key={nanoid()} className="text-red-700 text-md">
       #{tag}{' '}
     </p>
   ))
-
 
   return (
     <>
@@ -22,6 +33,7 @@ const PostPage = () => {
         {tags}
       </SHeader>
       <div className="py-5 mx-auto max-w-sm border-b-2">{post?.body}</div>
+      <div>{commentss}</div>
 
       {/* TODO: Add comments and comment form below the body*/}
     </>
