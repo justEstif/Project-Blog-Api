2c624f6f421e1dc9e2f7ad554836dc9067ff1cf9
Estifanos <estifanosbeyene21@gmail.com>
2022-09-22 06:28:30 -0400

    style(postpage): improve the layout of the page

--- client/src/pages/postpage/index.tsx
+++ client/src/pages/postpage/index.tsx
@@ -1,19 +1,32 @@
+import { nanoid } from 'nanoid'
 import SHeader from '../../components/SHeader'
 import { useLocation } from 'react-router-dom'
 import useGetPost from '../../hooks/useGetPost'
 
 const PostPage = () => {
   const postID = useLocation().state
-  const post = useGetPost(postID)
+  const post = useGetPost(postID) // the post info
 
-  // TODO: call the api and get post info
   return (
     <>
       <SHeader>
         <p className="text-5xl capitalize">{post?.title}</p>
+        <p className="uppercase text-md">
+          {post?.publicationDate?.toString() ?? 'Not Published'}
+        </p>
+
+        <div className="capitalize text-md">
+          {post?.tags.map((tag) => (
+            <p key={nanoid()} className="text-red-700 text-md">
+              #{tag}{' '}
+            </p>
+          ))}
+        </div>
       </SHeader>
-      <p>{post?.publicationDate?.toString() ?? "Not Published"}</p>
-      <p>{post?.body}</p>
+      <div className="py-5 mx-auto w-full max-w-sm border-b-2">
+        <p>{post?.body}</p>
+      </div>
+      {/* TODO: Add comments and comment form below the body*/}
     </>
   )
 }
