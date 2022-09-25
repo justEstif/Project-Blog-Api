1167896f1e311007ba1ab89dc1bce1086bbcb59c
Estifanos <estifanosbeyene21@gmail.com>
2022-09-25 16:18:25 -0400

    fix(getFormattedDate): fix function to format date

--- client/src/utils/getFormattedDate.ts
+++ client/src/utils/getFormattedDate.ts
@@ -1,7 +1,8 @@
 export default function getFormattedDate(date: Date) {
-  let year = date.getFullYear()
-  let day = date.getDate().toString().padStart(2, '0')
-  let month = date.toLocaleString('default', {
+  let passedDate = new Date(date.toString())
+  let year = passedDate.getFullYear()
+  let day = passedDate.getDate().toString().padStart(2, '0')
+  let month = passedDate.toLocaleString('default', {
     month: 'short'
   })
   return `${month} ${day}, ${year}`
