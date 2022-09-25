d7721f284267d2942e7aca84db3603203fc953fe
Estifanos <estifanosbeyene21@gmail.com>
2022-09-25 09:38:47 -0400

    feat: create working basic delete page

--- client/src/pages/deletepage/index.tsx
+++ client/src/pages/deletepage/index.tsx
@@ -1,10 +1,27 @@
+import { useNavigate } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import useDeletePost from '../../hooks/useDeletePost'
 
+interface IPostId {
+  postid: string
+}
 const DeletePage = () => {
-  const postID = useLocation().state
+  const { handleSubmit } = useForm<IPostId>()
+  const { setPostId } = useDeletePost()
+  const postId = useLocation().state
+  const navigate = useNavigate()
+  const onSubmit = handleSubmit((_) => {
+    setPostId(postId)
+    navigate('/owner')
+  })
   return (
     <div>
-      <div>This is the delete page: {postID}</div>
+      <form onSubmit={onSubmit}>
+        <button type="submit">Confirm</button>
+        <button>Cancel</button>
+      </form>
+      <div>This is the delete page: {}</div>
     </div>
   )
 }
