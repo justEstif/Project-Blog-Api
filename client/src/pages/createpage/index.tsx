b0ae96db913e331592e353581d39d3b4a53cf1d7
Estifanos <estifanosbeyene21@gmail.com>
2022-09-25 15:26:10 -0400

    feat: add basic form for creating post

--- client/src/pages/createpage/index.tsx
+++ client/src/pages/createpage/index.tsx
@@ -4,18 +4,26 @@ import useCreatePost from '../../hooks/useCreatePost'
 import ICreatePostProp from '../../interface/ICreatePostProp'
 
 const CreatePage = () => {
-  const { handleSubmit } = useForm<ICreatePostProp>()
+  const { register, handleSubmit } = useForm<ICreatePostProp>()
   const { setPost } = useCreatePost()
   const navigate = useNavigate()
+
   const onSubmit = handleSubmit((data) => {
     setPost(data)
     navigate('/owner')
   })
+
   return (
     <div>
       <div>This is the createpage</div>
       <form onSubmit={onSubmit}>
-        {/* TODO create form */}
+        <input type="text" {...register('title')} />
+        <textarea {...register('body')} />
+        <textarea {...register('summary')} />
+        <input type="text" {...register('tags')} />
+        <input type="checkbox" {...register('published')} />
+        {/* NOTE separate tags using comma */}
+        <button type='submit'>Submit</button>
       </form>
     </div>
   )
