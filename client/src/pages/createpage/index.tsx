2178f59e9dbed41eaafa45c84d996a5fb88ab876
Estifanos <estifanosbeyene21@gmail.com>
2022-09-25 12:31:27 -0400

    feat: add basic create page

--- client/src/pages/createpage/index.tsx
+++ client/src/pages/createpage/index.tsx
@@ -1,7 +1,22 @@
+import { useNavigate } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import useCreatePost from '../../hooks/useCreatePost'
+import ICreatePostProp from '../../interface/ICreatePostProp'
+
 const CreatePage = () => {
+  const { handleSubmit } = useForm<ICreatePostProp>()
+  const { setPost } = useCreatePost()
+  const navigate = useNavigate()
+  const onSubmit = handleSubmit((data) => {
+    setPost(data)
+    navigate('/owner')
+  })
   return (
     <div>
       <div>This is the createpage</div>
+      <form onSubmit={onSubmit}>
+        {/* TODO create form */}
+      </form>
     </div>
   )
 }
