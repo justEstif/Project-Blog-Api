4e7384a4d6bad67601c6820d00b06b47fd216bf5
Estifanos <estifanosbeyene21@gmail.com>
2022-09-26 06:39:43 -0400

    style: add better header

--- client/src/pages/deletepage/index.tsx
+++ client/src/pages/deletepage/index.tsx
@@ -4,6 +4,8 @@ import { useLocation } from 'react-router-dom'
 import useStore from '../../store'
 import { useForm } from 'react-hook-form'
 import useDeletePost from '../../hooks/useDeletePost'
+import SHeader from '../../components/SHeader'
+import useGetPost from '../../hooks/useGetPost'
 
 interface IPostId {
   postid: string
@@ -12,6 +14,7 @@ const DeletePage = () => {
   const { handleSubmit } = useForm<IPostId>()
   const { setDeleteData } = useDeletePost()
   const postId = useLocation().state
+  const { post } = useGetPost(postId)
   const navigate = useNavigate()
   const store = useStore((state) => state)
   const token = store.user?.token.token || ''
@@ -23,18 +26,25 @@ const DeletePage = () => {
     navigate('/owner')
   })
   return (
-    <div className="flex justify-center content-center">
-      <form onSubmit={onSubmit}>
-        <div className="flex gap-4">
-          <button type="submit" className="hover:bg-green-500">
-            Confirm
-          </button>
-          <Link to={`/owner`}>
-            <button>Cancel</button>
-          </Link>
-        </div>
-      </form>
-    </div>
+    <>
+      <SHeader>
+        <p className="text-5xl capitalize">Delete Post</p>
+        <p className="text-3xl text-slate-700">Title: {post?.title}</p>
+      </SHeader>
+
+      <div className="flex justify-center content-center">
+        <form onSubmit={onSubmit}>
+          <div className="flex gap-4">
+            <button type="submit" className="hover:bg-green-500">
+              Confirm
+            </button>
+            <Link to={`/owner`}>
+              <button>Cancel</button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </>
   )
 }
 
