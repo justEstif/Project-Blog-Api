2052108ffa935f2353f825fa4e5a2cf591de6d82
Estifanos <estifanosbeyene21@gmail.com>
2022-09-26 08:52:58 -0400

    style: improve the header of post page

--- client/src/pages/postpage/Header.tsx
+++ client/src/pages/postpage/Header.tsx
@@ -1,16 +1,19 @@
 import SHeader from '../../components/SHeader'
 import { nanoid } from 'nanoid'
+import getFormattedDate from '../../utils/getFormattedDate'
 import IPost from '../../interface/IPost'
 
 interface IProps {
   post: IPost | undefined
 }
 const Header = ({ post }: IProps) => {
-  const postPubDate = post?.publicationDate?.toString() ?? 'Not Published'
+  const postPubDate = post?.publicationDate
+    ? getFormattedDate(new Date(post?.publicationDate))
+    : 'Not Published'
 
   const postTags = post?.tags.map((tag) => (
     <p key={nanoid()} className="text-red-700 text-md">
-      #{tag}{' '}
+      #{tag}
     </p>
   ))
 
@@ -18,7 +21,7 @@ const Header = ({ post }: IProps) => {
     <SHeader>
       <p className="text-5xl capitalize">{post?.title}</p>
       <p className="uppercase text-md">{postPubDate}</p>
-      {postTags}
+      <div className="flex gap-3 justify-center content-center">{postTags}</div>
     </SHeader>
   )
 }
